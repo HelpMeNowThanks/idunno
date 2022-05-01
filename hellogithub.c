550e25c7cf5768bde1cca260629a83bb3c58fd8b
HelpMeNowThanks <manioelvstad@gmail.com>
2022-05-01 20:07:54 +0200

    actually did something

---
+++ hellogithub.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+
+int main() {
+    
+    while(1) {
+    printf("Hello, Github!\n");
+    sleep(1);
+    printf("bingbong\n");
+    sleep(1);
+    system("clear");
+    }
+
+    return 0;
+}
